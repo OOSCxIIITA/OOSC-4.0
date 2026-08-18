cf5993ab0a769cf3dae8dcdc3d8c47ccc4ecfb9b
AkshaTGA <parmarakshat17@gmail.com>
2026-08-18 21:03:41 +0530

    removes xss sanitization for acommodating for CSS styling in embedded html and added middleware for json

--- backend/server.js
+++ backend/server.js
@@ -68,13 +68,14 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   }),
 )
+app.use(express.json({limit:'1mb'}))
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use(cookieParser())
 
 // ── XSS Sanitization Middleware ────────────────────────────────────────────────
 const sanitizeInput = (obj) => {
   if (typeof obj === 'string') {
-    return xss(obj)
+    return obj
   }
   if (Array.isArray(obj)) {
     return obj.map(sanitizeInput)
